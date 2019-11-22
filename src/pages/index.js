53fd63f233cdc62ed60ad8bc9ae52e285e442344
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2019-11-21 22:18:53 -0600

    index grid setup & general layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,20 +15,35 @@ const IndexPage = () => (
         <div className="hero-interior-container">
           <div className="spin-container">
             <div className="hero-white-card">
-              <h1 className="hero-h1">I Make<br />Websites</h1>
+              <h1 className="hero-h1">i make websites</h1>
             </div>
           </div>
         </div>
       </div>
       <div className="main-body">
         <div className="main-content">
-          <h2>Here is a heading</h2>
-          <p>And this is a paragraph that goes along with that heading. We will see if I can get the code right to make it not look weird.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra et ultrices neque ornare. Elit at imperdiet dui accumsan. Dui ut ornare lectus sit. Commodo sed egestas egestas fringilla. Ut tortor pretium viverra suspendisse. Nec ultrices dui sapien eget mi. Bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim. Quisque egestas diam in arcu cursus euismod quis. Ipsum a arcu cursus vitae congue mauris rhoncus. Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Faucibus et molestie ac feugiat.</p>
-          <p>Hendrerit dolor magna eget est. Amet risus nullam eget felis eget. Aliquam id diam maecenas ultricies mi eget. Massa massa ultricies mi quis hendrerit dolor magna eget est. Augue interdum velit euismod in pellentesque massa placerat duis. Erat velit scelerisque in dictum non consectetur a. Ipsum dolor sit amet consectetur adipiscing. Enim nec dui nunc mattis enim ut. Diam quam nulla porttitor massa id neque aliquam vestibulum. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Elementum nisi quis eleifend quam. Praesent tristique magna sit amet purus gravida quis blandit. Ut consequat semper viverra nam. Dictum at tempor commodo ullamcorper.</p>
-          <p>Lacus vel facilisis volutpat est velit egestas dui. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Quam adipiscing vitae proin sagittis nisl rhoncus. Vitae sapien pellentesque habitant morbi tristique. Vitae suscipit tellus mauris a. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sed tempus urna et pharetra pharetra. Platea dictumst quisque sagittis purus. Ullamcorper a lacus vestibulum sed arcu non. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Tincidunt augue interdum velit euismod. Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus. Odio tempor orci dapibus ultrices in iaculis nunc sed. Vitae sapien pellentesque habitant morbi.</p>
-          <p>Consequat semper viverra nam libero justo. Tristique senectus et netus et malesuada fames ac turpis. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Vel fringilla est ullamcorper eget nulla facilisi. Feugiat in fermentum posuere urna nec tincidunt praesent. Eu augue ut lectus arcu bibendum at varius. Amet luctus venenatis lectus magna fringilla urna porttitor. In iaculis nunc sed augue lacus viverra vitae. Consectetur adipiscing elit duis tristique sollicitudin nibh. Et tortor at risus viverra adipiscing. Faucibus purus in massa tempor. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. In arcu cursus euismod quis. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
-          <p>Mi quis hendrerit dolor magna eget est lorem ipsum. Mattis aliquam faucibus purus in massa tempor nec feugiat. Adipiscing vitae proin sagittis nisl. Faucibus pulvinar elementum integer enim. Auctor urna nunc id cursus metus aliquam. Eu mi bibendum neque egestas congue quisque egestas diam in. Maecenas pharetra convallis posuere morbi. Orci eu lobortis elementum nibh tellus molestie nunc non blandit. Ac tincidunt vitae semper quis lectus. Velit egestas dui id ornare arcu. Eleifend mi in nulla posuere sollicitudin aliquam. Fringilla est ullamcorper eget nulla.</p>
+          <section>
+            <h2>my most recent stuff</h2>
+            <div className="large-grid">
+                <div className="grid-cell work-gradient-standard">
+                  <div className="grid-cell-interior">
+                    
+                  </div>
+                </div>
+                <div className="grid-cell articles-gradient-standard">
+                  
+                </div>
+                <div className="grid-cell contact-gradient-standard">
+                  
+                </div>
+            </div>
+          </section>
+          <section>
+            <h2>what i advocate for</h2>
+          </section>
+          <section>
+            <h2>more about me</h2>
+          </section>
         </div>
       </div>
     </div>
