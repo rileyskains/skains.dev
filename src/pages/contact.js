56b449f2f6a90fb00bf9c4a2663de881cfd81f66
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2019-12-24 21:19:55 -0600

    Contact page, image improvements, bug fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import rslogo from "../images/rs-logo.svg"
 
 const ContactPage = () => (
 
@@ -11,22 +11,24 @@ const ContactPage = () => (
 		<div>
 
 			<section className="hero--interior-page hero__background-gradient--contact">
-				<h1>Contact Me</h1>
+				<h1>Hello!</h1>
       		</section>
 
 			<div className="content-wrapper">
 
 				<section className="lead content-section">
-					<h2>Making the world better through websites.</h2>
-					<p>I create functional and accessible websites and web applications. Hire me to build a website, put together a design, or write an article.</p>
+					<p>I'm Riley Skains &ndash; a front-end web developer and designer based out of Huntsville, Texas. I work full-time as a web developer in the public sector, but my passion is in open source projects and bringing other people's ideas to life.</p>
 				</section>
 
-				<section className="contact-grid content-section">
+				<section className="content-section five-four-grid">
 					<div>
-						<p>I’m always looking to make friends and start new projects! Please consider hiring me for your next website!</p>
+						<div className="outline">
+							<p>If you want to contact me for hiring, want to collaborate on a project, or just feel like saying hi, please email me at <a href="mailto:riley@skains.dev">riley@skains.dev</a>.</p>
+							<p>For correspondence that is even more casual than that, you can find me on <a href="https://twitter.com/riley_skains">Twitter</a>.</p>
+						</div>
 					</div>
 					<div>
-						<Link to="contact" className="contact-button contact-button--contact">Contact</Link>
+						<img src={rslogo} alt="RS logo" />
 					</div>
 				</section>
 
