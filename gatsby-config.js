84ec1507e9e88bb25119858728a297ba5f820712
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2019-12-26 22:21:12 -0600

    Remove gatsby-plugin-feed for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-feed`,
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
