aaef8a66265b0d56ba47ea6d309974c46dbff424
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2020-03-01 20:48:48 -0600

    Anime page addition
    Adds anime.js page and anime.scss to support it.

---
+++ src/pages/anime.js
@@ -0,0 +1,134 @@
+import React from "react"
+
+import SEO from "../components/seo"
+import rslogo from "../images/rs-logo.svg"
+import "../styles/anime.scss"
+
+const AnimePage = () => (
+
+	<>
+    <SEO title="Anime" />
+		<div>
+        <div className="anime-body">
+        <h1>Anime This Week</h1>
+
+<div className="day-container">
+<h2>Sunday</h2>
+<div className="show-grid">
+  <div>
+    <h3>Boruto: Naruto Next Generations</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx97938-eqZi4WQk5wRU.jpg" alt="Boruto" />
+    
+  </div>
+  <div>
+    <h3>Ace of the Diamond: Second Season</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18689-HrvWZjpWvxMr.jpg" alt="Daiya" />
+    
+  </div>
+</div>
+</div>
+
+<div className="day-container">
+<h2>Monday</h2>
+<div className="show-grid">
+  <div>
+    <h3>Keep Your Hands Off Eizouken!</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx109298-YvjfI88hX76T.png" alt="Eizouken" />
+    
+  </div>
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+</div>
+</div>
+
+<div className="day-container">
+<h2>Tuesday</h2>
+<div className="show-grid">
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+</div>
+</div>
+
+
+<div className="day-container">
+<h2>Wednesday</h2>
+<div className="show-grid">
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+</div>
+</div>
+
+<div className="day-container">
+<h2>Thursday</h2>
+<div className="show-grid">
+  <div>
+    <h3>Ace of the Diamond: Second Season</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18689-HrvWZjpWvxMr.jpg" alt="Daiya" />
+    
+  </div>
+  <div>
+    <h3>The Case Files of Jeweler Richard</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx111951-vzX8hv9fl9pR.png" alt="Jeweler" />
+    
+  </div>
+</div>
+</div>
+
+
+<div className="day-container">
+<h2>Friday</h2>
+<div className="show-grid">
+  <div>
+    <h3>HAIKYU‼ TO THE TOP</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx106625-UR22wB2NuNVi.png" alt="Haikyuu" />
+    
+  </div>
+  <div>
+    <h3>Toilet-bound Hanako-kun</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx108463-knWkFUR311ho.jpg" alt="Hanako" />
+    
+  </div>
+</div>
+</div>
+<div className="day-container">
+<h2>Saturday</h2>
+<div className="show-grid">
+  <div>
+    <h3>My Hero Academia Season 4</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx104276-DplpGzgCoRZX.jpg" alt="Academia" />
+    
+  </div>
+  <div>
+    <h3>Magi: The Kingdom of Magic</h3>
+    <img src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx18115-bIjnYeBLOuNl.jpg" alt="Daiya" />
+    
+  </div>
+</div>
+</div>
+</div>
+		</div>
+
+	</>
+
+)
+
+export default AnimePage
