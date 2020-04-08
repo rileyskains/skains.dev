06a28ea65c01caa1b198e9f03feef4445b59f9fb
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2020-04-08 17:23:13 -0500

    Anime page Spring 2020 v1
    Anime page updates for spring 2020 season. Will need future adjustment due to changing info.

--- src/pages/anime.js
+++ src/pages/anime.js
@@ -3,6 +3,10 @@ import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 
+import crunchyroll from "../images/crunchyroll.svg"
+import funimation from "../images/funimation.svg"
+import netflix from "../images/netflix.svg"
+
 
 const AnimePage = () => (
 
@@ -22,11 +26,13 @@ const AnimePage = () => (
               <div className="shows-grid">
                 <div className="show-container">
                   <img src="https://img1.ak.crunchyroll.com/i/spire1/ffe5a84ffeafce5f20693f7e9b6ff0151579221442_full.jpg" alt="Boruto Show Poster" />
-                  <p>Boruto: Naruto Next Generations</p>
+                  <h3>Boruto: Naruto Next Generations</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire2/320673efcacdbdb0d6222156f8d797ce1504216291_full.jpg" alt="SNAFU Show Poster" />
-                  <p>My Teen Romantic Comedy SNAFU TOO!</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire2/39ab3edc4b030998d5122c2b6db9d12e1585964438_full.jpg" alt="SING Show Poster" />
+                  <h3>SING "YESTERDAY" FOR ME</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
               </div>
           </div>
@@ -35,12 +41,14 @@ const AnimePage = () => (
           <h2 class="anime-day-title">Monday</h2>
               <div className="shows-grid">
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire4/0147a7268ae1c79b111c8fd62fbe8fbe1433796778_full.jpg" alt="Ace of Diamond Show Poster" />
-                  <p>Ace of Diamond Act II</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire3/113723dbe198372b4cbede38898083991554421405_full.jpg" alt="Fruits Basket Show Poster" />
+                  <h3>Fruits Basket Season 2</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire3/71db9b8dbcbd69284df73b4c1e6ef3df1460066070_full.jpg" alt="Flying Witch Show Poster" />
-                  <p>Flying Witch</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire3/ec5a8f70373f31fdafcc3cf5328018621570198379_full.jpg" alt="Woodpecker Show Poster" />
+                  <h3>Woodpecker Detective's Office</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
               </div>
           </div>
@@ -49,26 +57,30 @@ const AnimePage = () => (
           <h2 class="anime-day-title">Tuesday</h2>
               <div className="shows-grid">
               <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire2/320673efcacdbdb0d6222156f8d797ce1504216291_full.jpg" alt="SNAFU Show Poster" />
-                  <p>My Teen Romantic Comedy SNAFU TOO!</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire4/0147a7268ae1c79b111c8fd62fbe8fbe1433796778_full.jpg" alt="Daiya Show Poster" />
+                  <h3>Ace of the Diamond Act II</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire3/71db9b8dbcbd69284df73b4c1e6ef3df1460066070_full.jpg" alt="Flying Witch Show Poster" />
-                  <p>Flying Witch</p>
+                  <img src="https://m.media-amazon.com/images/M/MV5BZmUzMThjOTItZGY4ZS00ODcwLTliNTMtYjVkM2JmY2QxNmRhXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg" alt="Violet Show Poster" />
+                  <h3>Violet Evergarden</h3>
+                  <img className="show-source" src={netflix} alt="Netflix" />
                 </div>
               </div>
           </div>
 
           <div className="single-anime-day">
           <h2 class="anime-day-title">Wednesday</h2>
-              <div className="shows-grid">
+          <div className="shows-grid">
               <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire4/0147a7268ae1c79b111c8fd62fbe8fbe1433796778_full.jpg" alt="Ace of Diamond Show Poster" />
-                  <p>Ace of Diamond Act II</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire4/0147a7268ae1c79b111c8fd62fbe8fbe1433796778_full.jpg" alt="Daiya Show Poster" />
+                  <h3>Ace of the Diamond Act II</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
                 <div className="show-container">
-                  <img src="https://m.media-amazon.com/images/M/MV5BZmUzMThjOTItZGY4ZS00ODcwLTliNTMtYjVkM2JmY2QxNmRhXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg" alt="Violet Evergarden Show Poster" />
-                  <p>Violet Evergarden</p>
+                  <img src="https://m.media-amazon.com/images/M/MV5BZmUzMThjOTItZGY4ZS00ODcwLTliNTMtYjVkM2JmY2QxNmRhXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg" alt="Violet Show Poster" />
+                  <h3>Violet Evergarden</h3>
+                  <img className="show-source" src={netflix} alt="Netflix" />
                 </div>
               </div>
           </div>
@@ -77,12 +89,14 @@ const AnimePage = () => (
           <h2 class="anime-day-title">Thursday</h2>
               <div className="shows-grid">
               <div className="show-container">
-                  <img src="https://m.media-amazon.com/images/M/MV5BZmUzMThjOTItZGY4ZS00ODcwLTliNTMtYjVkM2JmY2QxNmRhXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg" alt="Violet Evergarden Show Poster" />
-                  <p>Violet Evergarden</p>
+                  <img src="https://res.cloudinary.com/sfp/image/upload/q_60/cste/bf63d51c-a25a-47da-a5d6-91e33d46f0fc.jpg" alt="Kakushigoto Show Poster" />
+                  <h3>Kakushigoto</h3>
+                  <img className="show-source" src={funimation} alt="Funimation" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire3/71db9b8dbcbd69284df73b4c1e6ef3df1460066070_full.jpg" alt="Flying Witch Show Poster" />
-                  <p>Flying Witch</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire4/0147a7268ae1c79b111c8fd62fbe8fbe1433796778_full.jpg" alt="Daiya Show Poster" />
+                  <h3>Ace of the Diamond Act II</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
               </div>
           </div>
@@ -91,12 +105,14 @@ const AnimePage = () => (
           <h2 class="anime-day-title">Friday</h2>
               <div className="shows-grid">
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire4/a9c1f36167964b81371db1bde94087861580166621_full.jpg" alt="Haikyuu Show Poster" />
-                  <p>HAIKYU‼ TO THE TOP</p>
+                  <img src="https://res.cloudinary.com/sfp/image/upload/q_60/cste/6f27f824-cb1c-42b3-92ee-c40a53665b89.jpg" alt="Millionaire Show Poster" />
+                  <h3>The Millionaire Detective - Balance: UNLIMITED</h3>
+                  <img className="show-source" src={funimation} alt="Funimation" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire2/320673efcacdbdb0d6222156f8d797ce1504216291_full.jpg" alt="SNAFU Show Poster" />
-                  <p>My Teen Romantic Comedy SNAFU TOO!</p>
+                  <img src="https://m.media-amazon.com/images/M/MV5BZmUzMThjOTItZGY4ZS00ODcwLTliNTMtYjVkM2JmY2QxNmRhXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg" alt="Violet Show Poster" />
+                  <h3>Violet Evergarden</h3>
+                  <img className="show-source" src={netflix} alt="Netflix" />
                 </div>
               </div>
           </div>
@@ -105,12 +121,14 @@ const AnimePage = () => (
           <h2 class="anime-day-title">Saturday</h2>
               <div className="shows-grid">
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire2/cccce22d1dfaaf713284a617ee0b539a1572467883_full.jpg" alt="BNHA Show Poster" />
-                  <p>My Hero Academia Season 4</p>
+                  <img src="https://img1.ak.crunchyroll.com/i/spire4/bdb7cd339117868cd6696dce922f318e1571270456_full.jpg" alt="Food Wars Show Poster" />
+                  <h3>Food Wars: The Fifth Plate</h3>
+                  <img className="show-source" src={crunchyroll} alt="Crunchyroll" />
                 </div>
                 <div className="show-container">
-                  <img src="https://img1.ak.crunchyroll.com/i/spire2/320673efcacdbdb0d6222156f8d797ce1504216291_full.jpg" alt="SNAFU Show Poster" />
-                  <p>My Teen Romantic Comedy SNAFU TOO!</p>
+                  <img src="https://res.cloudinary.com/sfp/image/upload/q_60/cste/82a26997-8c3b-4379-8b2b-09a0803f9bc1.jpg" alt="Kaguya-sama Show Poster" />
+                  <h3>Kaguya-Sama: Love is War?</h3>
+                  <img className="show-source" src={funimation} alt="Funimation" />
                 </div>
               </div>
           </div>
